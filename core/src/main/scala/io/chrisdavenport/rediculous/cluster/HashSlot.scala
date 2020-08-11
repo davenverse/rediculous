6609b3311c994fc2a9ce4d306ba064f4ee72a165
Chris Davenport <chris@christopherdavenport.tech>
2020-08-11 15:03:06 -0700

    CRC16 initial attempt and HashSlot Initial Attempmpt

---
+++ core/src/main/scala/io/chrisdavenport/rediculous/cluster/HashSlot.scala
@@ -0,0 +1,18 @@
+package io.chrisdavenport.rediculous.cluster
+
+object HashSlot {
+  
+  def apply(key: String): Int = {
+    val toHash = hashKey(key)
+    CRC16(toHash)
+  }
+  
+  def hashKey(key: String): String = {
+    val s = key.indexOf('}')
+    if (s >= 0) {
+      val e = key.indexOf('}')
+      if (e >= 0 && e != s + 1) key.substring(s, e)
+      else key
+    } else key
+  }
+}
