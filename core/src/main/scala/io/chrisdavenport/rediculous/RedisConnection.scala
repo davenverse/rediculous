3d2c809ac834720a9d48463e23f9ca52914addf1
Christopher Davenport <chris@christopherdavenport.tech>
2020-08-11 21:11:14 -0700

    Add Comment Examples of the failing cases

--- core/src/main/scala/io/chrisdavenport/rediculous/RedisConnection.scala
+++ core/src/main/scala/io/chrisdavenport/rediculous/RedisConnection.scala
@@ -24,7 +24,7 @@ object RedisConnection{
   ) extends RedisConnection[F]
   private case class DirectConnection[F[_]](socket: Socket[F]) extends RedisConnection[F]
 
-  private case class Cluster[F[_]](queue: Chunk[(Deferred[F, Either[Throwable, Resp]], Option[String], Resp)] => F[Unit]) extends RedisConnection[F]
+  private case class Cluster[F[_]](queue: Chunk[(Deferred[F, Either[Throwable, Resp]], Option[String], Option[(String, Int)], Resp)] => F[Unit]) extends RedisConnection[F]
 
   // Guarantees With Socket That Each Call Receives a Response
   // Chunk must be non-empty but to do so incurs a penalty
@@ -73,7 +73,7 @@ object RedisConnection{
           c.traverse(_._1.get).flatMap(_.sequence.traverse(l => Sync[F].delay(l.toNel.getOrElse(throw new Throwable("Rediculous: Impossible Return List was Empty but we guarantee output matches input"))))).rethrow
         }   
       }
-      case Cluster(queue) => chunk.traverse(resp => Deferred[F, Either[Throwable, Resp]].map((_, key, resp))).flatMap{ c => 
+      case Cluster(queue) => chunk.traverse(resp => Deferred[F, Either[Throwable, Resp]].map((_, key, None, resp))).flatMap{ c => 
         queue(c).as {
           c.traverse(_._1.get).flatMap(_.sequence.traverse(l => Sync[F].delay(l.toNel.getOrElse(throw new Throwable("Rediculous: Impossible Return List was Empty but we guarantee output matches input"))))).rethrow
         }
@@ -153,7 +153,7 @@ object RedisConnection{
     for {
       sockets <- Resource.liftF(single(sg, new InetSocketAddress(host, port)).use(ClusterCommands.clusterslots[Redis[F, *]].unRedis.run(_).flatten))
       refTopology <- Resource.liftF(Ref[F].of(sockets))
-      queue <- Resource.liftF(Queue.bounded[F, Chunk[(Deferred[F, Either[Throwable,Resp]], Option[String], Resp)]](maxQueued))
+      queue <- Resource.liftF(Queue.bounded[F, Chunk[(Deferred[F, Either[Throwable,Resp]], Option[String], Option[(String, Int)], Resp)]](maxQueued))
       keypool <- KeyPoolBuilder[F, (String, Int), (Socket[F], F[Unit])](
         {t: (String, Int) => sg.client[F](new InetSocketAddress(t._1, t._2)).allocated},
         { case (_, shutdown) => shutdown}
@@ -167,13 +167,13 @@ object RedisConnection{
               Stream.eval(refTopology.get).map{topo => 
                 Stream.eval(topo.random[F]).flatMap{ default => 
                 Stream.emits(
-                    chunk.toList.groupBy{ case (_, s, _) => // TODO Investigate Efficient Group By
-                    s.flatMap(key => topo.served(HashSlot.find(key))).getOrElse(default)
+                    chunk.toList.groupBy{ case (_, s, server,_) => // TODO Investigate Efficient Group By
+                    server.orElse(s.flatMap(key => topo.served(HashSlot.find(key)))).getOrElse(default)
                   }.toSeq
                 ).evalMap{
                   case (server, rest) => 
                     keypool.map(_._1).take(server).use{m =>
-                      val out = Chunk.seq(rest.map(_._3))
+                      val out = Chunk.seq(rest.map(_._4))
                       explicitPipelineRequest(m.value, out).attempt.flatTap{// Currently Guarantee Chunk.size === returnSize
                         case Left(_) => m.canBeReused.set(Reusable.DontReuse)
                         case _ => Applicative[F].unit
@@ -182,18 +182,18 @@ object RedisConnection{
                     case Right(n) => 
                       n.zipWithIndex.parTraverseN(10){
                         case (ref, i) => 
-                          val v@(toSet, _, _) = rest(i)
+                          val v@(toSet, _, _, _) = rest(i)
                           ref match {
-                            case Resp.Error(s) if (s.startsWith("MOVED")) => 
+                            case Resp.Error(s) if (s.startsWith("MOVED")) => // MOVED 1234-2020 127.0.0.1:6381
                               refreshTopology >> cluster.queue(Chunk.singleton(v))
-                            case Resp.Error(s) if (s.startsWith("ASK")) => 
+                            case Resp.Error(s) if (s.startsWith("ASK")) => //ASK 1234-2020 127.0.0.1:6381
                               toSet.complete(Either.right(ref)) // TODO Handle Ask Redirection (explicit?) Send Asked and then send
                             case otherwise => 
                               toSet.complete(Either.right(otherwise))
                           }
                       }.void
                     case e@Left(_) => 
-                      rest.traverse_{ case (deff, _, _) => deff.complete(e.asInstanceOf[Either[Throwable, Resp]])}
+                      rest.traverse_{ case (deff, _, _, _) => deff.complete(e.asInstanceOf[Either[Throwable, Resp]])}
                   }
 
                 }
